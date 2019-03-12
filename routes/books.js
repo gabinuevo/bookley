b76265e1db9510d7ed7cae6d91a7d38a2c0e7fcf
annika <annikaslund@gmail.com>
2019-03-12 15:35:51 -0700

    add functionality to /books/:isbn PATCH validator

--- routes/books.js
+++ routes/books.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = new express.Router();
 const Book = require("../models/book");
+const ExpressError = require("../expressError");
 const jsonschema = require("jsonschema");
-const bookSchema = require("../schemas/bookSchema.json");
+const createBookSchema = require("../schemas/createBookSchema.json");
+const updateBookSchema = require("../schemas/updateBookSchema.json")
 
 router.get("/", async function (req, res, next) {
   try {
@@ -24,7 +26,7 @@ router.get("/:id", async function (req, res, next) {
 
 router.post("/", async function (req, res, next) {
   try {
-    const result = jsonschema.validate(req.body, bookSchema);
+    const result = jsonschema.validate(req.body, createBookSchema);
 
     if (!result.valid) {
       // pass validation errors to error handler
@@ -35,8 +37,8 @@ router.post("/", async function (req, res, next) {
     }
 
     const book = await Book.create(req.body);
-    å
-    return res.status(201).json({ book });
+    return res.status(201).json({ book: book });
+    
   } catch (err) {
     return next(err);
   }
@@ -44,7 +46,19 @@ router.post("/", async function (req, res, next) {
 
 router.patch("/:isbn", async function (req, res, next) {
   try {
-    const book = await Book.update(req.params.isbn, req.body);
+    const result = jsonschema.validate(req.body, updateBookSchema);
+
+    if (!result.valid) {
+      // pass validation errors to error handler
+      //  (the "stack" key is generally the most useful)
+      let listOfErrors = result.errors.map(error => error.stack);
+      let error = new ExpressError(listOfErrors, 400);
+      return next(error);
+    }
+    const targetBook = await Book.findOne(req.params.isbn);
+    debugger;
+    const updatedData = {...targetBook, ...req.body}
+    const book = await Book.update(req.params.isbn, updatedData);
     return res.json({ book });
   } catch (err) {
     return next(err);
